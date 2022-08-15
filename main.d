7fd7d83f263f1fca5d538019b99c964660588ad3
Alinne Alessandra <69517722+alinnealess@users.noreply.github.com>
2022-08-15 18:22:31 -0300

    Delete main.d

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o main.d : main.c types.h constants.h strings.h interfaces/checkers.h \
-  interfaces/../constants.h interfaces/counters.h interfaces/../types.h \
-  interfaces/inits.h interfaces/io.h interfaces/recursion.h
